390962a5b164a55a0c631598d00fca78f002cad7
Dima Sibirykov <sibiriakoff2006@yandex.ru>
2018-11-06 21:13:04 +0300

    commit 2

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-// Печатает год в STDOUT
+header('X-My-Header: hi');
+
+// ПРобаж
+echo "SERVER \n";
+/*array_map(function ($item) {
+	print_r($item);
+	echo "\n";
+}, $_SERVER);
+ */
+print_r($_POST) . "\n";
+echo "GET \n";
 echo date('Y');
+
+echo "Hello world";
