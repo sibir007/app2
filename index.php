809fcc4020a90aca13986aaaaaa715ed358a70e1
Dima Sibirykov <sibiriakoff2006@yandex.ru>
2018-11-07 10:37:42 +0300

    end PHP CGI

--- index.php
+++ index.php
@@ -1,18 +1,41 @@
 <?php
-header('X-My-Header: hi');
+/*header('X-My-Header: hi');
 
 // ПРобаж
 echo "SERVER \n";
-/*array_map(function ($item) {
+array_map(function ($item) {
 	print_r($item);
 	echo "\n";
 }, $_SERVER);
- */
-print_r($_REQUEST) . "\n";
+
+
+//print_r($GENERAL) . "\n";
 echo "GET \n";
 echo date('Y');
 
+header("Status: 404 Not Found");
+//http_response_code(404);
 echo "Hello world";
 if ($_SERVER['REQUEST_URI'] != '/'){
 	echo 'Page not found. <a href="/">main</a>';
-};
+	//header("Status: 404 Not Found");
+	http_response_code(404);
+};	
+
+ */
+//<?php
+
+// BEGIN (write your solution here)
+switch ($_SERVER['REQUEST_URI']) {
+    case '/':
+        echo '<a href="/welcome">welcome</a>
+        <br><a href="/not-found">not-found</a>
+        break';
+    case '/welcome':
+        echo '<a href="/">main</a>';
+        break;        
+    default:
+        http_response_code(404);
+        echo 'Page not found. <a href="/">main</a>';
+}
+// END
